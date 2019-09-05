70c29a78672478e429e19cc09f26c3cab0bd0c2e
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2019-09-05 15:44:35 -0300

    Initial Rails 6 migration

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: bundle exec puma -C config/puma.rb
-worker: bundle exec sidekiq -c 5 -v
-release: bundle exec rake db:migrate
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,2 @@
 class ApplicationController < ActionController::API
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
-
-  # i18n configuration. See: http://guides.rubyonrails.org/i18n.html
-  before_action :set_locale
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-
-  def default_url_options
-    { locale: locale }
-  end
-
-  # for devise to redirect with locale
-  def self.default_url_options(options = {})
-    options.merge(locale: I18n.locale)
-  end
-
-  def index
-    render json: { message: 'Welcome to Rails Graph QL Bootstrap' }
-  end
 end
