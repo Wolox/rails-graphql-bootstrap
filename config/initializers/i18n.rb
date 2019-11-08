3467c00fbe509068cf117dd3dda1c288707415c0
Marcelo Guzman <marcegl91@gmail.com>
2019-11-08 18:02:58 -0300

    Fix restrictive locale loading

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -4,6 +4,6 @@ Rails.application.configure do
   # explicitly tell Rails to look further.
   config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
-  # Restrict which locales should be loaded by rails-i18n.
-  config.i18n.available_locales = :en
+  # Restrict which locales should be loaded by rails-i18n. By default loads all locales.
+  # config.i18n.available_locales = :en
 end
